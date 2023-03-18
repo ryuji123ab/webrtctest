490944f69441934ab15c575ef51a3e93ed28c155
ryuji123ab <128220642+ryuji123ab@users.noreply.github.com>
2023-03-18 19:45:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const captureButton = document.getElementById('capture');
 const resultElement = document.getElementById('result');
 const ctx = canvas.getContext('2d');
 
-navigator.mediaDevices.getUserMedia({  facingMode: 'user',video: { width: 720, height: 1280 }, audio: false })
+navigator.mediaDevices.getUserMedia({  video: { width: 720, height: 1280 ,facingMode: 'user'}, audio: false })
     .then(stream => {
         video.srcObject = stream;
         video.play();
